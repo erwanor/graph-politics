2372c348b1997191e80323199d460388cdcaf0ae
Aaron <aaron.ounn@gmail.com>
2016-09-30 17:05:51 -0400

    Fix typo and path to dataset generation

--- upload2graph/uploader.py
+++ upload2graph/uploader.py
@@ -1,9 +1,15 @@
 from neo4j.v1 import GraphDatabase, basic_auth
 import os
 
+CRD_PATH      = os.path.dirname(os.path.realpath(__file__))
+CONGRESS_LIST = ''.join([CRD_PATH, '/../processed_data/congress/legislators_excerpt.json'])
+
+def upload_congress():
+	return
+
 db_username = os.environ["NEO4J_USERNAME"]
 db_password = os.environ["NEO4J_PASSWORD"]
-db_server   = os.environ["NEO4_SERVER_URI"]
+db_server   = os.environ["NEO4J_SERVER_URI"]
 
 driver = GraphDatabase.driver(db_server, auth = basic_auth(db_username, db_password))
 
