bb899c987ff1024e268afff770582a0796f44b07
Aaron <aaron.ounn@gmail.com>
2016-09-29 21:51:12 -0400

    Add basic structure to connect to the db

--- upload2graph/uploader.py
+++ upload2graph/uploader.py
@@ -1,6 +1,10 @@
+from neo4j.v1 import GraphDatabase, basic_auth
 import os
 
 db_username = os.environ["NEO4J_USERNAME"]
 db_password = os.environ["NEO4J_PASSWORD"]
-db_server   = os.environ["NEO4_SERVER"]
+db_server   = os.environ["NEO4_SERVER_URI"]
 
+driver = GraphDatabase.driver(db_server, auth = basic_auth(db_username, db_password))
+
+session.close()
