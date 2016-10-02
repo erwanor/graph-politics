be557aca8c971f5ed366aa74b02768165eff33a8
Aaron <aaron.ounn@gmail.com>
2016-10-01 23:19:06 -0400

    Revamp the graph uploader to use the py2neo driver

--- upload2graph/uploader.py
+++ upload2graph/uploader.py
@@ -1,27 +1,48 @@
-from neo4j.v1 import GraphDatabase, basic_auth
-import os
+from py2neo import authenticate as graph_db_auth, Graph, Node, Relationship
+from os import environ as env_vars
+from os.path import dirname, realpath
 import json
 
-CRD_PATH              = os.path.dirname(os.path.realpath(__file__))
+# Generate path to dataset using the current reading diretory's realpath
+CRD_PATH              = dirname(realpath(__file__))
 CONGRESS_LIST_DATASET = ''.join([CRD_PATH, '/../processed_data/congress/congress_list_excerpt.json'])
 
+# Graph database authentication infos are stored in environment variables
+NEO4J_SERVER_PORT = ':'.join([env_vars['NEO4J_SERVER_NAME'], env_vars['NEO4J_SERVER_PORT']])
+NEO4J_DB_USERNAME = env_vars['NEO4J_DB_USERNAME']
+NEO4J_DB_PASSWORD = env_vars['NEO4J_DB_PASSWORD']
+
 def store_file_in_memory(path):
+	'''Store file content in memory and in a single line'''
 	with open(path, 'r') as srcfile:
 		file_string = srcfile.read().replace('\n', '')
 	return file_string
 
-def chamber_noun(chamber):
+def check_whitespace(string):
+	'''Add double quotes to string containing whitespaces'''
+	add_double_quotes = False
+	for char in string:
+		if char == ' ':
+			add_double_quotes = True
+	if add_double_quotes is True:
+		return ''.join(['"', string, '"'])
+	else:
+		return string
+
+def chamber_name(chamber):
+	'''Capitalize chamber names'''
 	if chamber == 'house':
-		return 'Representative'
+		return 'House of Representatives'
 	elif chamber == 'senate':
-		return 'Senator'
+		return 'Senate'
 	else:
 		return 'Official'
 
-def party_name(abr):
-	if abr == 'R':
+def party_name(abrv):
+	'''Replace party abreviation by full party name'''
+	if abrv == 'R':
 		return 'Republican'
-	elif abr == 'D':
+	elif abrv == 'D':
 		return 'Democrat'
 	else:
 		return 'Independent'
@@ -32,34 +53,52 @@ def remove_redundant_entries(politician):
 	del politician['state']
 	return
 
-def format_dict_to_str(hashtable):
-	string = '{'
-	for k in hashtable:
-		string = ''.join([string, k, ':', '"', hashtable[k], '"', ','])
-	string = ''.join([string[:-1], '}'])
-	return string
-
-def upload_congress(session):
-	congress_json = store_file_in_memory(CONGRESS_LIST_DATASET)
-	congress_data = json.loads(congress_json)
-	for chamber in congress_data:
-		for politician in congress_data[chamber]:
-			chamber_cap = chamber_noun(chamber)
-			party       = party_name(politician['party'])
-			state       = politician['state']
-			remove_redundant_entries(politician)
-			politician_str = format_dict_to_str(politician)
-			CYPHER_QUERY = ''.join(['CREATE (n: Politician:Congress:', chamber_cap, ':', party, politician_str, ')'])
-			session.run(CYPHER_QUERY)
+def clear_graph(graph):
+	'''Delete all nodes and relationships from the graph databse'''
+	QUERY_QUEUE = ['MATCH (n)-[r]-() DELETE n,r', 'MATCH (n) DELETE n']
+	for CYPHER_QUERY in QUERY_QUEUE:
+		graph.run(CYPHER_QUERY)
+	print 'Cleared graph'
 	return
 
-db_username = os.environ['NEO4J_USERNAME']
-db_password = os.environ['NEO4J_PASSWORD']
-db_server   = os.environ['NEO4J_SERVER_URI']
+def main_political_nodes(roots, graph):
+	'''Create main political nodes/relationships; graph building blocks'''
+	store = {}
+	for entry in roots:
+		store.update({ entry[1]: Node(entry[0], name=entry[1]) })
+		graph.create(store[entry[1]])
+		print 'Create Node -  ', entry[0], ':', entry[1]
 
-driver  = GraphDatabase.driver(db_server, auth = basic_auth(db_username, db_password))
-session = driver.session()
+	graph.create(Relationship(store['Senate'], 'IS_A_CHAMBER_OF', store['Congress']))
+	graph.create(Relationship(store['House of Representatives'], 'IS_A_CHAMBER_OF', store['Congress']))
+	return store
 
-upload_congress(session)
+def build_congress(root_nodes, graph):
+	'''Generate congress nodes/relationships from the congress list dataset'''
+	congress_json = store_file_in_memory(CONGRESS_LIST_DATASET)
+	congress_data = json.loads(congress_json)
+	# Hacky #######
+	P_UID = 0
+	# TO REMOVE ###
+	for chamber in congress_data:
+		for politician in congress_data[chamber]:
+			#P_UID = RANDOM
+			P_UID = P_UID + 1
+			firstName      = politician['first_name']
+			lastName       = politician['last_name']
+			fullName       = ' '.join([firstName, lastName])
+			chamberName    = chamber_name(chamber)
+			partyName      = party_name(politician['party'])
+			remove_redundant_entries(politician)
+			politicianNode = Node('Politician', full_name=fullName, PUID=P_UID)
+			graph.create(politicianNode)
+			graph.create(Relationship(politicianNode, 'MEMBER_OF', root_nodes[chamberName]))
+			graph.create(Relationship(politicianNode, 'MEMBER_OF', root_nodes['Congress']))
+			graph.create(Relationship(politicianNode, 'MEMBER_OF', root_nodes[partyName]))
 
-session.close()
+graph_db_auth(NEO4J_SERVER_PORT, NEO4J_DB_USERNAME, NEO4J_DB_PASSWORD)
+graph = Graph()
+clear_graph(graph)
+root_nodes = main_political_nodes([('Institution', 'Congress'), ('Chamber', 'Senate'), ('Chamber', 'House of Representatives'), 
+	('Party', 'Republican'), ('Party', 'Democrat'), ('Party', 'Independent')], graph)
+build_congress(root_nodes, graph)
