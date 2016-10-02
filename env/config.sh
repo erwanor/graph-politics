7d370bac5d8f79aad2073c51ba61fb12850c5927
Aaron <aaron.ounn@gmail.com>
2016-10-01 23:22:17 -0400

    Update graph auth environment variables

--- env/config.sh
+++ env/config.sh
@@ -1,4 +1,8 @@
 # Default Neo4j configuration
-export NEO4J_USERNAME="neo4j"
-export NEO4J_PASSWORD="neo4j"
-export NEO4J_SERVER_URI="bolt://localhost/"
+export NEO4J_SERVER_NAME="localhost"
+export NEO4J_SERVER_PORT="7474"
+# Default username/password combination
+# Update the content of the following variables with
+#        your username/password:
+export NEO4J_DB_USERNAME="neo4j"
+export NEO4J_DB_PASSWORD="neo4j"
